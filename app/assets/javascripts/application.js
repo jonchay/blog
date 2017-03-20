79929d85c9df7a5d898ca078c6071ae94887a2e2
Jonathan Chay <jonchay@gmail.com>
2017-03-19 23:01:54 -0700

    Adding tether for Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery/dist/jquery
+//= require tether/dist/js/tether
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap/dist/js/bootstrap
+//= require home
